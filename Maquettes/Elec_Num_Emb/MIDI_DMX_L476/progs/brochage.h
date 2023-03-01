9a3dd0b6c540b707173af5fc7abaf5dee4a0c6fa
Julien Villemejane <julien.villemejane@institutoptique.fr>
2023-03-01 08:19:29 +0100

    Mise à jour brochage Robot Holonome

---
+++ Maquettes/Elec_Num_Emb/MIDI_DMX_L476/progs/brochage.h
@@ -0,0 +1,24 @@
+/****************************************************************************/
+/*  Board Pinout                                                            */
+/*      + DMX (output only) : PE_8, PE_7, PE_12, PE_10, PE_14               */
+/*              (uart7 - tx, rx, out_tx, start, enable)                     */
+/*      + MIDI1 : PB_9, PB_8 (uart5)                                        */
+/*      + MIDI2 : PG_14, PG_9 (uart6)                                       */
+/*      + MIDI3 (input only) : PE_1, PE_0 (uart8)                           */
+/*      + Analog R,G,B : PF_8, PF_7, PF_9                                   */
+/*      + Analog Pitch - Volume : PB_1, PC_2                                */
+/*      + Analog In 1 & 2 : PA_6, PA_7                                      */
+/*      + LCD : PB_5, PB_4, PB_3, PB_13, PF_13                              */
+/*              (mosi, miso, sck, cs, rs)                                   */
+/*              EA DOG LCD / 3 lines / SPI mode / 5V                        */
+/*      + SD : PE_6, PE_5, PE_2, PE_3    (SPI4)                             */
+/*              (mosi, miso, sck, cs)                                       */
+/*      + nRF module : PB_5, PB_4, PB_3, PB_15, PC_7, PA_15                 */
+/*              (mosi, miso, sck, cs, ce, irq)                              */
+/*      + MSGEQ7 : PD_14, PD_15, PA_4                                       */
+/*              (strobe, reset, analog out)                                 */
+/*      + WS2812 led out : PC_8                                             */
+/*      + DAC1 out : PA_5                                                   */
+/*      + HC-05 BT module : PD_1, PD_0 (uart4)                              */
+/*      + Digital In Pull-Down 1 to 4 : PA_7, PF_2, PF_1, PF_0              */
+/****************************************************************************/
